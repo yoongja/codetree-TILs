c1ec46564eaafcf83f0937823fccb3588442d9b0
Yoonji Kim <83260809+yoongja@users.noreply.github.com>
2024-05-18 16:01:53 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240518/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240518/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -8,16 +8,15 @@ for i in range(n):
 min_dis = 1e15 #최단거리
 
 for i in range(1, n-1):
-    #현재 건너뛰는 체크포인트 첫번째꺼 마지막꺼 제외!
-    #print(graph[i]) 
     dis = 0 
-    for j in range(n-2):
+    prev = 0
+    for j in range(1,n):
         if i == j: # 건너뛰는 것과 같은 인덱스면? 
-            dis += abs(graph[i-1][0] - graph[i+1][0]) + abs(graph[i-1][1] - graph[i+1][1])
-            #print(dis)
-        else: #같은 인덱스가 아니라면?
-            dis += abs(graph[i-1][0] - graph[i][0]) + abs(graph[i-1][1] - graph[i][1])
-            #print(dis)
+            continue # 무시
+        #그렇지 않다면 전에꺼랑 계산해주기
+        dis += abs(graph[prev][0] - graph[j][0]) + abs(graph[prev][1] - graph[j][1])
+        prev = j #인덱스 바꿔주기
+
     min_dis = min(min_dis, dis)
 
 print(min_dis)
