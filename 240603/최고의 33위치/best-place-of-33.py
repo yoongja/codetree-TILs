577e1c843fc63b3bd0656b0bc5f0f4c117f2ee4e
Yoonji Kim <83260809+yoongja@users.noreply.github.com>
2024-06-03 13:10:14 +0900

    Title: 최고의 33위치, Time: 240ms, Memory: 25MB, Status: Passed - CodeTree

--- 240603/최고의 33위치/best-place-of-33.py
+++ 240603/최고의 33위치/best-place-of-33.py
@@ -3,22 +3,19 @@ n = int(input())
 graph = [list(map(int,input().split())) for _ in range(n)]
 
 # 3 * 3 크기 격자 내에 들어올 최대 동전의 수
-def calculate_coin(y, x):
+def calculate_coin(s_y, s_x, e_y, e_x):
     earn_coin = 0
-    for i in range(3):
-        for j in range(3):
-            earn_coin += graph[y + i][x + j]
+    for i in range(s_y, e_y + 1):
+        for j in range(s_x, e_x + 1):
+            earn_coin += graph[i][j]
     return earn_coin
-
 
 money = 0
 max_money = 0
 
 for y in range(n - 2):
     for x in range(n - 2):
-        if abs(y - x) <= 2:
-
-            money = calculate_coin(y, x)
-            max_money = max(money, max_money)
+        money = calculate_coin(y, x, y + 2, x + 2)
+        max_money = max(money, max_money)
 
 print(max_money)
