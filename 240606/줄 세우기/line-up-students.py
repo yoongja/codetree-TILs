df3bb0d182b492b6e0ddf1052ca8a5c2c9ebeb7c
Yoonji Kim <83260809+yoongja@users.noreply.github.com>
2024-06-06 01:49:16 +0900

    Title: 줄 세우기, Time: 184ms, Memory: 25MB, Status: Passed - CodeTree

--- 240606/줄 세우기/line-up-students.py
+++ 240606/줄 세우기/line-up-students.py
@@ -4,6 +4,7 @@ for i in range(n):
     lst.append(tuple(map(int,input().split())))
     lst[i]+=(i+1,)
 
-lst.sort(key=lambda x:(x[0],x[1],x[2]), reverse=True)
+lst.sort(key=lambda x:(x[0],x[1]),reverse=True)
+
 for h,w,n in lst:
     print(f"{h} {w} {n}")
