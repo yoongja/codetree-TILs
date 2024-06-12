dd873b2375f7bedeb663fb367402a14f9b0dc986
Yoonji Kim <83260809+yoongja@users.noreply.github.com>
2024-06-12 15:20:04 +0900

    Title: 특정 수와 근접한 합, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240612/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -0,0 +1,14 @@
+n, s = map(int, input().split())
+l = list(map(int, input().split()))
+
+min_val = 1e15
+
+for i in range(n):
+    for j in range(n):
+        if l[i] == l[j]:
+            continue
+        tmp = sum(l) - l[i] - l[j]
+        minus = abs(s - tmp)
+        min_val = min(min_val, minus)
+
+print(min_val)
