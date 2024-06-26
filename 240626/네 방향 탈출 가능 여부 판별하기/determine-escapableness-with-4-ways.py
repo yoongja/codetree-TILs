17eea299036cd7b483109e5dfb2ec7e355736185
Yoonji Kim <83260809+yoongja@users.noreply.github.com>
2024-06-26 17:16:28 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240626/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,34 @@
+from collections import deque
+q=deque()
+n,m = tuple(map(int,input().split()))
+graph=[
+    list(map(int,input().split()))
+    for i in range(n)
+]
+visited=[[0 for x in range(m)]for _ in range(n)]
+
+def can_go(x,y):
+    if x<0 or y<0 or x>=m or y>=n:
+        return False
+    if graph[x][y]==0 or visited[x][y]==1:
+        return False
+    return True
+    
+
+dxs,dys=[1,0,-1,0],[0,1,0,-1]
+def bfs():
+    while q:
+        x,y=q.popleft()
+        for dx,dy in zip(dxs,dys):
+            new_x,new_y=x+dx,y+dy
+            if can_go(new_x,new_y):
+                visited[new_x][new_y]=1
+                q.append((new_x,new_y))
+
+q.append((0,0))
+bfs()
+
+if visited[-1][-1]:
+    print(1)
+else:
+    print(0)
