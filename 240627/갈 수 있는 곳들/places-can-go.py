d683e30e52f695ab79a92191c2fda38e8f6e909a
Yoonji Kim <83260809+yoongja@users.noreply.github.com>
2024-06-27 11:17:16 +0900

    Title: 갈 수 있는 곳들, Time: 213ms, Memory: 26MB, Status: Passed - CodeTree

--- 240627/갈 수 있는 곳들/places-can-go.py
+++ 240627/갈 수 있는 곳들/places-can-go.py
@@ -11,26 +11,26 @@ for i in range(len(point)):
     # 출발하는 좌표를 넣어준다
     y = point[i][0] - 1
     x = point[i][1] - 1
-    #print("시작좌표", y,x)
-    
+
     if not visited[y][x]:
         q.append([y,x])
-        ans += 1
         visited[y][x] = True
-    else:
-        break
+        ans += 1
+        #print("시작좌표", y,x)
+    
 
     dx = [0, 0, 1, -1]
     dy = [1,-1, 0, 0]
 
     while q:
         nowy, nowx = q.popleft() #지금 좌표
+
         # 0만 갈 수 있음
         for i in range(4):
             nx = nowx + dx[i]
             ny = nowy + dy[i]
             if 0 <= nx < n and 0 <= ny < n and arr[ny][nx] == 0 and not visited[ny][nx]:
-                #print("방문하는 좌표", nx, ny, arr[ny][nx])
+                #print("방문하는 좌표", ny, nx)
                 ans += 1
                 q.append([ny,nx])
                 visited[ny][nx] = True
