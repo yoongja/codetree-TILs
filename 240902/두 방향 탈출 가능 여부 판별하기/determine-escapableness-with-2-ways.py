ec3cceaaa63441a396d6430a3b73f0655b2921bc
Yoonji Kim <83260809+yoongja@users.noreply.github.com>
2024-09-02 20:04:22 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240902/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240902/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -13,18 +13,15 @@ def dfs(x, y):
     for d in dir:
         nx = x + d[0]
         ny = y + d[1]
-        if 0 <= nx < n and 0 <= ny < m:
-            if l[nx][ny] == 1 and not visited[nx][ny]:
+        if 0 <= nx < n and 0 <= ny < m:  # 인덱스가 유효한지 먼저 확인
+            if l[nx][ny] == 1 and not visited[nx][ny]:  # 이후에 방문 및 값 체크
                 visited[nx][ny] = 1
                 dfs(nx, ny)
 
-# 시작점이 1인 경우에만 탐색을 시작합니다.
-if l[0][0] == 1:
-    visited[0][0] = 1
-    dfs(0, 0)
-
-# 끝점(n-1, m-1)에 도달할 수 있는지 확인
-if visited[n - 1][m - 1] == 1:
+visited[0][0] = 1
+dfs(0, 0)
+if visited[n-1][m-1] == 1:
     print(0)
 else:
     print(1)
+#print(visited[n - 1][m - 1])
