2126e631c907d3492a442b5c192a3959c77fee38
Yoonji Kim <83260809+yoongja@users.noreply.github.com>
2024-10-09 22:47:45 +0900

    Title: 선택 정렬 구현, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 241009/선택 정렬 구현/implement-selection-sort.py
+++ 241009/선택 정렬 구현/implement-selection-sort.py
@@ -1,9 +1,9 @@
 n = int(input())
 lst = list(map(int,input().split()))
 
-for i in range(n-1):
+for i in range(n):
     minimum = i
-    for l in range(i,n-1):
+    for l in range(i,n):
         if lst[l]<lst[minimum]:
             minimum=l
     temp = lst[i]
